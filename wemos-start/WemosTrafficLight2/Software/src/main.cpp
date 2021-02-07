8b3ae08651698b4d2749072b2dea478ae280b903
Maurice Snoeren <mac.snoeren@avans.nl>
2021-02-07 11:49:59 +0100

    Working version with auto recovery

--- wemos-start/WemosTrafficLight2/Software/src/main.cpp
+++ wemos-start/WemosTrafficLight2/Software/src/main.cpp
@@ -112,6 +112,22 @@ void callbackMQTT(char* topic, byte* pl, unsigned int length) {
   Serial.println("MQTT EVENT '" + String(topic) + "': " + payload);
 }
 
+void connectMQTT() {
+  while (!mqtt.connected()) {
+    mqtt.connect("iwsn-wemos-client-maurice");
+    mqtt.subscribe("iwsn-wemos-event");
+    if ( mqtt.connected() ) {
+      Serial.println("MQTT CONNECTED!");
+      payload = "{ \"id\": \"maurice\", \"mqtt\": \"iwsn-wemos-client-maurice\", \"ip\": \"" + WiFi.localIP().toString() + "\"}";
+      mqtt.publish("iwsn-wemos", payload.c_str());
+
+    } else {
+      Serial.println("MQTT NOT CONNECTED!");
+    }
+    delay(500);
+  }
+}
+
 void setup() {
   // Get the mode of the leds
   pinMode(LIGHT_1_RED,    OUTPUT);
@@ -153,12 +169,12 @@ void setup() {
   fsm.addState(STATE_OUT_OF_ORDER,   preOutOfOrder, loopOutOfOrder, postOutOfOrder);
 
   // Add the events to the FSM
-  fsm.addTransition(STATE_START, EVENT_STATE_EXECUTED, STATE_LIGHT_1_GREEN);
-  fsm.addTransition(STATE_LIGHT_1_GREEN, EVENT_TIMER, STATE_LIGTH_1_ORANGE);
+  fsm.addTransition(STATE_START, EVENT_START, STATE_LIGHT_1_GREEN);
+  //fsm.addTransition(STATE_LIGHT_1_GREEN, EVENT_TIMER, STATE_LIGTH_1_ORANGE); // Demo, auto to ligth 2
   fsm.addTransition(STATE_LIGHT_1_GREEN, EVENT_BUTTON_2, STATE_LIGTH_1_ORANGE);
   fsm.addTransition(STATE_LIGTH_1_ORANGE, EVENT_TIMER, STATE_LIGTH_1_RED);
   fsm.addTransition(STATE_LIGTH_1_RED, EVENT_TIMER, STATE_LIGHT_2_GREEN);
-  fsm.addTransition(STATE_LIGHT_2_GREEN, EVENT_TIMER, STATE_LIGTH_2_ORANGE);
+  //fsm.addTransition(STATE_LIGHT_2_GREEN, EVENT_TIMER, STATE_LIGTH_2_ORANGE); // Demo, auto to light 1
   fsm.addTransition(STATE_LIGHT_2_GREEN, EVENT_BUTTON_1, STATE_LIGTH_2_ORANGE);
   fsm.addTransition(STATE_LIGTH_2_ORANGE, EVENT_TIMER, STATE_LIGTH_2_RED);
   fsm.addTransition(STATE_LIGTH_2_RED, EVENT_TIMER, STATE_LIGHT_1_GREEN);
@@ -183,6 +199,24 @@ void setup() {
   fsm.addTransition(STATE_LIGTH_2_RED,    EVENT_ERROR, STATE_START);
   fsm.addTransition(STATE_OUT_OF_ORDER,   EVENT_ERROR, STATE_START);
 
+  WiFi.begin("MaCMaN_GUEST", "GUEST@MACMAN"); // Connect to the Wi-Fi (if not known use WifiManager from tzapu!)
+  Serial.print("Setup Wi-Fi:");
+  while ( WiFi.status() != WL_CONNECTED ) {
+      delay(500);
+      Serial.print(".");
+  }
+  WiFi.setAutoReconnect(true);
+  WiFi.persistent(true);
+  Serial.println();
+  Serial.print("Connected:");
+  Serial.println(WiFi.localIP());
+
+  mqtt.setClient(client); // Setup the MQTT client
+  mqtt.setBufferSize(2048); // override MQTT_MAX_PACKET_SIZE
+  mqtt.setCallback(callbackMQTT);
+  mqtt.setServer(MQTT_SERVER, MQTT_PORT);
+  connectMQTT();
+
   fsm.setup(STATE_START, EVENT_STATE_EXECUTED);
   timerMQTT = millis();
 }
@@ -195,7 +229,7 @@ void loop() {
     fsm.raiseEvent(EVENT_ERROR);
   }
 
-  if ( millis() - timerMQTT > 1000 ) { // Sent every second the loop timing of the wemos device
+  if ( millis() - timerMQTT > 5000 ) { // Sent every five second the loop timing of the wemos device
     payload = "{ \"id\": \"maurice\", \"looptiming\": " + String(fsm.getLoopTime()) + "}";
     mqtt.publish("iwsn-wemos", payload.c_str());
     timerMQTT = millis();
@@ -214,35 +248,27 @@ void preStart() {
 }
 
 void loopStart() {
-  if ( WiFi.status() != WL_CONNECTED ) { // Check the wifi connection
-    WiFi.begin("MaCMaN_GUEST", "GUEST@MACMAN"); // Connect to the Wi-Fi (if not known use WifiManager from tzapu!)
-    Serial.print("Setup Wi-Fi:");
-    while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
+  if ( WiFi.status() == WL_CONNECTED ) {
+    if ( !mqtt.connected() ) {
+      connectMQTT();
     }
-    Serial.println();
-    Serial.print("Connected:");
-    Serial.println(WiFi.localIP());
-    payload = "{ \"id\": \"maurice\", \"ip\": \"" + WiFi.localIP().toString() + "\"}";
-    mqtt.publish("iwsn-wemos", payload.c_str());
-  }
-  
-  if ( !mqtt.connected() ) { // Check the mqtt connection
-    mqtt.setClient(client); // Setup the MQTT client
-    mqtt.setBufferSize(2048); // override MQTT_MAX_PACKET_SIZE
-    mqtt.setCallback(callbackMQTT);
-    mqtt.setServer(MQTT_SERVER, MQTT_PORT);
-    mqtt.connect("iwsn-wemos-client-maurice");
-    mqtt.subscribe("iwsn-wemos-event");
+    fsm.raiseEvent(EVENT_START);
 
-    if ( mqtt.connected() ) {
-      Serial.println("MQTT CONNECTED!");
-      payload = "{ \"id\": \"maurice\", \"mqtt\": \"iwsn-wemos-client-maurice\"}";
-      mqtt.publish("iwsn-wemos", payload.c_str());
-  
-    } else {
-      Serial.println("MQTT NOT CONNECTED!");
+  } else {
+    Serial.println("WiFi reconnection");
+    WiFi.begin("MaCMaN_GUEST", "GUEST@MACMAN"); // Connect to the Wi-Fi (if not known use WifiManager from tzapu!)
+    //WiFi.reconnect();
+    for ( unsigned int i=0; i < 20; ++i ) {
+      if ( WiFi.status() != WL_CONNECTED ) {
+        delay(1000);
+        Serial.print(".");
+          Serial.println();
+          Serial.print("Connected:");
+          Serial.println(WiFi.localIP());
+
+      } else {
+        i = 20;
+      }
     }
   }
 }
@@ -273,7 +299,6 @@ void loopLight1Green() {
 }
 
 void postLight1Green() {
-  Serial.println("Post Ligt1Green");
 }
 
 void preLight1Orange() {
